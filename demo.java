ec10dc141aacdf02301a48a2bc3c29f79d05e69a
AdithyanBongo <147474079+AdithyanBongo@users.noreply.github.com>
2023-10-10 11:07:34 +0530

    Add files via upload

---
+++ demo.java
@@ -0,0 +1,15 @@
+package boy.edu;
+
+public class demo {
+
+	public static void main(String[] args) {
+		int a=50;
+		int b=60;
+		int c=a;
+		a=b;
+		b=c;
+		System.out.println(a+" "+b);
+
+	}
+
+}
